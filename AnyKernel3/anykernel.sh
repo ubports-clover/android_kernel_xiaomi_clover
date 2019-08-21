cee877604940b0f447c282908043ab8b54b705b1
1582130940 <1582130940@qq.com>
2019-08-21 19:13:33 +0800

    build: Add AnyKernel3 & build script
    * LICENSE: slight clarifications and touch-ups

---
+++ AnyKernel3/anykernel.sh
@@ -0,0 +1,40 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Xtreme Kernel for Xiaomi Clover by 1582130940
+do.devicecheck=1
+do.modules=1
+do.cleanup=1
+do.cleanuponabort=1
+device.name1=clover
+device.name2=
+device.name3=
+device.name4=
+device.name5=
+supported.versions=
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 755 755 $ramdisk/init* $ramdisk/sbin;
+
+## AnyKernel install
+dump_boot;
+
+write_boot;
+
+## end install
